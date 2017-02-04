06cd01c24263ca81015a5495b0b4a166b0d1f5cc
Zach Nolan <zach@uplinkconsulting.com>
2017-02-03 23:45:36 -0800

    styling for tablet and desktop

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var header = document.getElementById('header');
+var intro = document.getElementById('intro');
+
+window.onscroll = function() {
+  var y = (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+
+  if (y > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+
+  if (y >= intro.offsetHeight - 60) {
+    header.classList.add('reveal');
+  } else {
+    header.classList.remove('reveal');
+  }
+}
